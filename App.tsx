eeeeade9c064ee4515124d3247be3108822dbbed
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-08-03 16:36:21 -0300

    feat: context

--- App.tsx
+++ App.tsx
@@ -16,7 +16,9 @@ import {
 
 import theme from './src/global/styles/theme';
 
-import { AppRoutes } from './src/routes/app.routes';
+// import { AppRoutes } from './src/routes/app.routes';
+import { SignIn } from './src/screens/SignIn';
+import { AuthProvider } from './src/hooks/auth';
 
 
 export default function App() {
@@ -39,7 +41,9 @@ export default function App() {
           backgroundColor="transparent"
           translucent
         />
-        <AppRoutes />
+        <AuthProvider>
+          <SignIn />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
   );
---
+++ src/hooks/auth.tsx
@@ -0,0 +1,38 @@
+import React , {createContext, ReactNode, useContext} from 'react';
+
+interface AuthProviderProps {
+  children : ReactNode;
+}
+
+interface User {
+  id: string;
+  name: string;
+  email: string;
+  photo?: string;
+}
+
+interface IAuthContextData {
+  user: User;
+}
+
+const AuthContext = createContext({} as IAuthContextData);
+
+function AuthProvider({children}: AuthProviderProps){
+  const user = {
+    id: '1234',
+    name: 'Gabriel Marques',
+    email: 'gabriel.mfrahm@hotmail.com'
+  }
+  return(
+    <AuthContext.Provider value={{user}}>
+      {children}
+    </AuthContext.Provider>
+  )
+}
+
+function useAuth(){
+  const context = useContext(AuthContext);
+  return context;
+}
+
+export {AuthProvider, useAuth}
