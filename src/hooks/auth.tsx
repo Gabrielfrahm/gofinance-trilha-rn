8c6b90d3862a48946f0830a6542bb46d479d8537
Gabriel Marques <gabriel_frahm@hotmail.com>
2021-08-04 12:03:07 -0300

    feat: social login apple

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -4,7 +4,8 @@ const { CLIENT_ID } = process.env;
 const { REDIRECT_URL } = process.env;
 
 import * as AuthSession from 'expo-auth-session';
-
+import * as AppleAuthentication from 'expo-apple-authentication';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 interface AuthProviderProps {
   children: ReactNode;
 }
@@ -19,6 +20,7 @@ interface User {
 interface IAuthContextData {
   user: User;
   signInWithGoogle(): Promise<void>;
+  signInWithApple(): Promise<void>;
 }
 
 interface AuthorizationResponse {
@@ -57,8 +59,31 @@ function AuthProvider({ children }: AuthProviderProps) {
     }
   }
 
+  async function signInWithApple() {
+    try {
+      const credential = await AppleAuthentication.signInAsync({
+        requestedScopes: [
+          AppleAuthentication.AppleAuthenticationScope.EMAIL,
+          AppleAuthentication.AppleAuthenticationScope.FULL_NAME
+        ]
+      });
+
+      if(credential){
+        const userLogged = {
+          id: String(credential.user),
+          email: credential.email!,
+          name: credential.fullName!.givenName!,
+        };
+        setUser(userLogged)
+        await AsyncStorage.setItem('@gofinances:user', JSON.stringify(userLogged));
+      }
+    } catch (error) {
+      throw new Error(error);
+    }
+  }
+
   return (
-    <AuthContext.Provider value={{ user, signInWithGoogle }}>
+    <AuthContext.Provider value={{ user, signInWithGoogle, signInWithApple }}>
       {children}
     </AuthContext.Provider>
   )
